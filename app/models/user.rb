4f123743bf9f2dc140a8ceb8510e8820ec92b0c2
shinodak01 <liberty1738@gmail.com>
2020-12-27 09:36:46 +0900

    エラーメッセージ機能、ログインログアウトの条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :birthday_date,  presence: true
 end
